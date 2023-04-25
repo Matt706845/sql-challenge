2db4578136a0355a61a51fb27f94d466c7a13634
Matt706845 <124213934+Matt706845@users.noreply.github.com>
2023-04-24 22:43:49 -0400

    Add files via upload

---
+++ titles table.sql
@@ -0,0 +1,15 @@
+Create Table titles(
+title_id Varchar (7),
+title Varchar(7) Not Null
+);
+
+Select * from titles;
+
+insert into titles (title_id, title) 
+Values (s0001,'Staff'),
+(s0002,'Senior Staff'),
+(e0001,'Assistant Engineer'),
+(e0002,'Engineer'),
+(e0003,'Senior Engineer'),
+(e0004,'Technique Leader'),
+(m0001,'Manager');
